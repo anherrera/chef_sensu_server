ee964c9e742d14c441e646f291913e9ba31778ed
Alexandra Herrera <alexa.herrera90@gmail.com>
2017-01-03 10:20:54 -0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,4 +10,4 @@ include_recipe 'sensu::default'
 
 include_recipe "sensu::server_service"
 include_recipe "sensu::api_service"
-include_recipe "sensu::dashboard_service"
+#include_recipe "sensu::dashboard_service"
